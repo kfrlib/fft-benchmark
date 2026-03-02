af34063fdc0afefc3e0a8461d14324e339ebee3c
d.levin256@gmail.com <d.levin256@gmail.com>
2026-03-02 12:55:59 +0100

    Remove performs_scaling

--- src/benchmark.hpp
+++ src/benchmark.hpp
@@ -344,8 +344,6 @@ public:
     virtual ~fft_impl() {}
     virtual void execute(real* out, const real* in) = 0;
 
-    virtual bool performs_scaling() const { return false; }
-
     constexpr static bool valid = true;
 };
 
