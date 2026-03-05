db8953cb3eeb8881d900d73785ae68101a7c8202
d.levin256@gmail.com <d.levin256@gmail.com>
2026-03-05 21:20:29 +0100

    JUCE SRC support

--- libs/juce/juce.cpp
+++ libs/juce/juce.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "fft_benchmark.hpp"
+#include "src_benchmark.hpp"
 #include <cstring>
 #include <juce_dsp/juce_dsp.h>
 #include <string>
@@ -97,3 +98,48 @@ fft_impl_ptr<real> fft_create(const std::vector<size_t>& size, bool is_complex, 
 
 template std::unique_ptr<fft_impl<float>> fft_create<float>(const std::vector<size_t>&, bool, bool, bool);
 template std::unique_ptr<fft_impl<double>> fft_create<double>(const std::vector<size_t>&, bool, bool, bool);
+
+std::string src_name()
+{
+    return "JUCE " + std::string(JUCE_STRINGIFY(JUCE_MAJOR_VERSION)) + "." +
+           std::string(JUCE_STRINGIFY(JUCE_MINOR_VERSION)) + "." +
+           std::string(JUCE_STRINGIFY(JUCE_BUILDNUMBER)) + " (WindowedSinc)";
+}
+
+template <typename real>
+struct src_implementation;
+
+template <>
+struct src_implementation<float> : public src_impl<float>
+{
+    src_implementation(unsigned out_rate, unsigned in_rate, unsigned seconds)
+    {
+        out_length = out_rate * seconds;
+        in_length  = in_rate * seconds;
+        speed_ratio = static_cast<double>(in_rate) / static_cast<double>(out_rate);
+        interpolator.reset();
+    }
+
+    void execute(float* out, const float* in) final
+    {
+        interpolator.reset();
+        interpolator.process(speed_ratio, in, out, static_cast<int>(out_length));
+    }
+
+    juce::WindowedSincInterpolator interpolator;
+    double speed_ratio;
+    size_t out_length;
+    size_t in_length;
+};
+
+template <typename real>
+src_impl_ptr<real> src_create(unsigned out_rate, unsigned in_rate, unsigned seconds)
+{
+    if constexpr (std::is_same_v<real, float>)
+        return src_impl_ptr<float>(new src_implementation<float>(out_rate, in_rate, seconds));
+    else
+        return nullptr; // JUCE interpolators only support float
+}
+
+template std::unique_ptr<src_impl<float>> src_create<float>(unsigned, unsigned, unsigned);
+template std::unique_ptr<src_impl<double>> src_create<double>(unsigned, unsigned, unsigned);
